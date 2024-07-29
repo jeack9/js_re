76e62f1f391f2943dfd887b7f9d779fa7f6b8a12
jeack9 <jeack99@g.yju.ac.kr>
2024-07-29 22:48:45 +0900

    승자 tictactoe

---
+++ node-wrok/tictactoe/src/tictacto.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+export default function Board() {  
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isTurnX, setIsTurnX] = useState(true); // 첫 턴: X의 턴
+  function handleClick(i) {
+    // 중복 위치 방지 || 승자확인
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice(); // 진행 히스토리
+    if (isTurnX) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setIsTurnX(!isTurnX); // 턴 변경
+  }
+  
+  const winner = calculateWinner(squares);
+  let status = winner ? `Winner: ${winner}` : `Next Player: ${isTurnX ? "X" : "O"}`;
+  
+  function calculateWinner(squares){
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ];
+    for(let i = 0; i < lines.length; i++){
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
+      </div>
+    </>
+  );
+}
