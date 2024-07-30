5fdf17d6f594cb58dcc275620ee3e4e2f9545f2d
jeack9 <jeack99@g.yju.ac.kr>
2024-07-31 00:06:12 +0900

    tictactoe

--- node-wrok/tictactoe/src/Tictactoe.jsx
+++ node-wrok/tictactoe/src/Tictactoe.jsx
@@ -7,46 +7,29 @@ function Square({ value, onSquareClick }) {
     </button>
   );
 }
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isTurnX, setIsTurnX] = useState(true); // 첫 턴: X의 턴
-  function handleClick(i) {
+function Board({isTurnX, squares, onPlay}) {
+  function handleClick(i) {    
     // 중복 위치 방지 || 승자확인
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
+    console.log(isTurnX, "xTrun");
     const nextSquares = squares.slice(); // 진행 히스토리
     if (isTurnX) {
+      console.log("xx", i);
       nextSquares[i] = "X";
     } else {
+      console.log("oo", i);
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setIsTurnX(!isTurnX); // 턴 변경
+    onPlay(nextSquares);
   }
 
+  // 승자출력
   const winner = calculateWinner(squares);
   let status = winner ? `Winner: ${winner}` : `Next Player: ${isTurnX ? "X" : "O"}`;
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
+  
   return (
     <>
       <div className="status">{status}</div>
@@ -67,4 +50,71 @@ export default function Board() {
       </div>
     </>
   );
+}
+export default function Game(){
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  // const [isTurnX, setIsTurnX] = useState(true); // 턴 스테이트
+  const isTurnX = currentMove % 2 === 0;
+  // 이동한 히스토리가 현재 스퀘어 정보
+  let currentSquares = history[currentMove];
+  // 게임히스토리, 턴정보 저장 자식컴포넌트에서 호출, 현재 히스토리 번호
+  function handlePlay(nextSquares){ 
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    console.log(nextHistory, "nexthist");
+    setHistory(nextHistory, 'next');
+    // setHistory([...history.slice(0, currentMove + 1), nextSquares], 'next');
+    console.log(history, "currhis");
+    // setCurrentMove(history.length - 1);
+    setCurrentMove(nextHistory.length - 1);
+    console.log(currentMove, "currmove");
+    // console로 찍었을때 이전값이 찍히는 이유 생명주기? qna
+    // setIsTurnX(!isTurnX);
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    // setIsTurnX(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move > 0){
+      description = "Go to move #" + move;
+    }else{
+      description = "Go to game start";
+    }
+    return (<li key={move}>
+      <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>)
+  });
+
+  return <div className="game">
+    <div className="game-board">
+      <Board isTurnX={isTurnX} squares={currentSquares} onPlay={handlePlay}/>
+    </div>
+    <div className="game-info">
+      <ol>{moves}</ol>
+    </div>
+  </div>
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
